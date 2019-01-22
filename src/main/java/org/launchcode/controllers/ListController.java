f9bf3459a73045c047b38ef9fd0eeccacc4187b0
Jacob Dozier <jacobadozier@gmail.com>
2019-01-21 21:29:22 -0600

    Calculated and passed the number of results to the list view.

--- src/main/java/org/launchcode/controllers/ListController.java
+++ src/main/java/org/launchcode/controllers/ListController.java
@@ -37,16 +37,25 @@ public class ListController {
     @RequestMapping(value = "values")
     public String listColumnValues(Model model, @RequestParam String column) {
 
+        Integer numJobs;
+        String stringNumJobs;
+
         if (column.equals("all")) {
             ArrayList<HashMap<String, String>> jobs = JobData.findAll();
+            numJobs = jobs.size();
+            stringNumJobs = numJobs.toString() + " Result(s)";
             model.addAttribute("title", "All Jobs");
             model.addAttribute("jobs", jobs);
+            model.addAttribute("stringNumJobs", stringNumJobs);
             return "list-jobs";
         } else {
             ArrayList<String> items = JobData.findAll(column);
+            numJobs = items.size();
+            stringNumJobs = numJobs.toString() + " Result(s)";
             model.addAttribute("title", "All " + columnChoices.get(column) + " Values");
             model.addAttribute("column", column);
             model.addAttribute("items", items);
+            model.addAttribute("stringNumJobs", stringNumJobs);
             return "list-column";
         }
 
@@ -56,9 +65,15 @@ public class ListController {
     public String listJobsByColumnAndValue(Model model,
             @RequestParam String column, @RequestParam String value) {
 
+        Integer numJobs;
+        String stringNumJobs;
+
         ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(column, value);
+        numJobs = jobs.size();
+        stringNumJobs = numJobs.toString() + " Result(s)";
         model.addAttribute("title", "Jobs with " + columnChoices.get(column) + ": " + value);
         model.addAttribute("jobs", jobs);
+        model.addAttribute("stringNumJobs", stringNumJobs);
 
         return "list-jobs";
     }
